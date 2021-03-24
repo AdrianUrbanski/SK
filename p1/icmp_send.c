60b8dc142d4dac484f87cf23cbd3c3cc78c9f990
Adrian Urbański <adrianu161999@gmail.com>
2021-03-24 18:43:25 +0100

    Start writing send function

---
+++ p1/icmp_send.c
@@ -0,0 +1,53 @@
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "icmp_checksum.h"
+
+struct icmp create_header (int seq) {
+	struct icmp header;
+	header.icmp_type = ICMP_ECHO;
+	header.icmp_code = 0;
+	header.icmp_hun.ih_idseq.icd_id = getpid();
+	header.icmp_hun.ih_idseq.icd_seq = seq;
+	header.icmp_cksum = 0;
+	header.icmp_cksum = compute_icmp_checksum(
+			(u_int16_t*) &header, sizeof(header));
+	
+	return header;
+}
+
+
+ssize_t send_packet (int sockfd, struct icmp header, char* ip_addr) {
+
+	struct sockaddr_in	recipient;
+	socklen_t		recipient_len = sizeof(recipient);
+	bzero (&recipient, recipient_len);
+	recipient.sin_family = AF_INET;
+	inet_pton(AF_INET, ip_addr, &recipient.sin_addr);
+
+	ssize_t bytes_sent = sendto (
+			sockfd,
+			&header,
+			sizeof(header),
+			0,
+			(struct sockaddr*)&recipient,
+			recipient_len
+		);
+	if (bytes_sent < 0) {
+		fprintf(stderr, "sendto error: %s\n", strerror(errno));
+		return EXIT_FAILURE
+	}
+
+	return bytes_sent;
+}
+
+int main(){
+	return 0;
+}
